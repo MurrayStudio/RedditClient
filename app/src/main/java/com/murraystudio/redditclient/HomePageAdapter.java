c165da21b940031f0db5afe3ed5dddfe0d493e44
Murray Studio <sushishamus@gmail.com>
2016-11-03 19:41:37 -0700

    Added recycler view which will hold the home page's posts from Reddit servers.

---
+++ app/src/main/java/com/murraystudio/redditclient/HomePageAdapter.java
@@ -0,0 +1,56 @@
+package com.murraystudio.redditclient;
+
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by sushi_000 on 11/3/2016.
+ */
+
+public class HomePageAdapter extends RecyclerView.Adapter<HomePageAdapter.MyViewHolder> {
+
+    private String[] mDataset;
+
+    // Provide a reference to the views for each data item
+    // Complex data items may need more than one view per item, and
+    // you provide access to all the views for a data item in a view holder
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+        public CardView mCardView;
+        public TextView mTextView;
+        public MyViewHolder(View v) {
+            super(v);
+
+            mCardView = (CardView) v.findViewById(R.id.card_view);
+            mTextView = (TextView) v.findViewById(R.id.tv_text);
+        }
+    }
+
+    // Provide a suitable constructor (depends on the kind of dataset)
+    public HomePageAdapter(String[] myDataset) {
+        mDataset = myDataset;
+    }
+
+    @Override
+    public HomePageAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            // create a new view
+            View v = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.home_page_card_view, parent, false);
+            // set the view's size, margins, paddings and layout parameters
+            MyViewHolder vh = new MyViewHolder(v);
+            return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(HomePageAdapter.MyViewHolder holder, int position) {
+        holder.mTextView.setText(mDataset[position]);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mDataset.length;
+    }
+}
--- app/src/main/java/com/murraystudio/redditclient/MainActivity.java
+++ app/src/main/java/com/murraystudio/redditclient/MainActivity.java
@@ -5,9 +5,9 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -26,6 +26,28 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        // Check that the activity is using the layout version with
+        // the fragment_container FrameLayout
+        if (findViewById(R.id.fragment_container) != null) {
+
+            // However, if we're being restored from a previous state,
+            // then we don't need to do anything and should return or else
+            // we could end up with overlapping fragments.
+            if (savedInstanceState != null) {
+                return;
+            }
+
+            // Create a new Fragment to be placed in the activity layout
+            HomePage homePageFragment = new HomePage();
+
+            // Replace whatever is in the fragment_container view with this fragment,
+            // and add the transaction to the back stack so the user can navigate back
+            getFragmentManager().beginTransaction().replace(R.id.fragment_container, homePageFragment).addToBackStack(null).commit();
+
+        }
+
+
     }
 
     @Override
